2c2dc454434ab228d02a400046599d38b7044da7
Michelle Hickner <mhickner@uw.edu>
2020-03-02 14:19:04 -0800

    Adds threshold parameter for masking NaNs in plots of spatial modes

--- project_main.m
+++ project_main.m
@@ -18,6 +18,8 @@ subplot(1,2,1)
 imagesc(Y)
 title('Original data')
 
+%% mean-subtract and fill in NaNs for SVD
+
 Yavg = mean(Y,2,'omitnan'); % compute row mean for subtraction
 Yms = Y-Yavg*ones(1,size(Y,2)); % Y mean-subtracted
 Yfill = fillmissing(Yms, 'constant', 0); %replace NaN
@@ -26,7 +28,16 @@ imagesc(Yfill)
 title('Mean-subtracted, NaNs filled in')
 
 [U,S,V] = svd(Yfill,'econ');
+%% replace NaNs
 
+Ynan = ismissing(Y);
+nanRow = sum(Ynan,2); % number of NaNs in each row
+th = 4; % threshold for number of NaNs allowed before row is masked
+for iRow = 1:size(Y,1)
+    if nanRow(iRow) > th
+        U(iRow,:) = NaN;
+    end
+end
 %% show modes
 
 figure
