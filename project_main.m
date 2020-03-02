81a0d741f69772cb0d9002126c616eb5bee626d6
Michelle Hickner <mhickner@uw.edu>
2020-03-02 13:26:23 -0800

    Changes the order of averaging and NaN fill, adds a few plots.

--- project_main.m
+++ project_main.m
@@ -8,28 +8,37 @@ nx = 52;
 ny = 52;
 Y = zeros(nx*ny, nRot);
 for iRot = 1:nRot
-    Y(:,iRot) = reshape(data.(rotFields{iRot}).Interp.Vort_crop, [nx*ny 1]);
+    %Y(:,iRot) = reshape(data.(rotFields{iRot}).Interp.Vort_crop, [nx*ny 1]);
+    %Y(:,iRot) = reshape(data.(rotFields{iRot}).InterpCommon.Vmag_crop, [nx*ny 1]);
+    Y(:,iRot) = reshape(data.(rotFields{iRot}).Interp.Vmag_crop, [nx*ny 1]);
 end
 
-Yavg = mean(Y,2,'omitnan'); % compute row mean for subtraction
 figure
 subplot(1,2,1)
-imagesc(reshape(Yavg,[nx ny]))
-title('average vorticity')
-%colMean = mean(Y,1,'omitnan'); %compute column mean for NaN substitution
-Yavg = fillmissing(Yavg, 'constant', 0); %replace NaN
+imagesc(Y)
+title('Original data')
+
+Yavg = mean(Y,2,'omitnan'); % compute row mean for subtraction
+Yms = Y-Yavg*ones(1,size(Y,2)); % Y mean-subtracted
+Yfill = fillmissing(Yms, 'constant', 0); %replace NaN
 subplot(1,2,2)
-imagesc(reshape(Yavg,[nx ny]))
-title('average vorticity, NaNs filled')
+imagesc(Yfill)
+title('Mean-subtracted, NaNs filled in')
 
-Yfill = fillmissing(Y, 'constant', 0); %replace NaN
-
-[U,S,V] = svd(Yfill-Yavg*ones(1,size(Y,2)),'econ');
+[U,S,V] = svd(Yfill,'econ');
 
 %% show modes
 
 figure
+subplot(1,5,1)
+imagesc(reshape(Yavg,[nx ny]))
+title('mean')
 for k = 1:4
-    subplot(1,4,k)
+    subplot(1,5,k+1)
     imagesc(reshape(U(:,k), [nx ny]))
+    title({'mode ',k})
 end
+
+figure
+imagesc(V(:,1:4))
+title('V')
