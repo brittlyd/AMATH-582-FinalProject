029bc941f15a63a76bfcd5efc6f3ba82058619a3
abigales <abigales@uw.edu>
2020-03-05 16:54:39 -0800

    modified to fix the downstream data

--- rename.m
+++ rename.m
@@ -1,7 +1,8 @@
 %Rename the fields from U and V to u and v
-names=fieldnames(up1_1FULL);
+names=fieldnames(down1_1FULL);
 for n=1:length(names)
-    names2=lower(fieldnames(up1_1FULL.(names{n})));
+    names2=lower(fieldnames(down1_1FULL.(names{n})));
     %replace the fieldnames
-    up1_1FULL.(names{n})=cell2struct(struct2cell(up1_1FULL.(names{n})),names2);
+    down1_1FULL.(names{n})=cell2struct(struct2cell(down1_1FULL.(names{n})),names2);
 end
+save('down1_1FULL','down1_1FULL')
