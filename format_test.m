d31f0a412bee795905bd7d4f8619bb135e872cd4
Michelle Hickner <mhickner@uw.edu>
2020-03-09 09:33:19 -0700

    Updates format_test to match inputs of stackPCA

--- format_test.m
+++ format_test.m
@@ -4,14 +4,16 @@ load('up1_1Full.mat')
 
 %%
 %%% Pull data out of structure
-u = up1_1FULL.deg_30.U(:,:,end);
-v = up1_1FULL.deg_30.V(:,:,end);
+u = up1_1FULL.deg_30.u(:,:,end);
+v = up1_1FULL.deg_30.v(:,:,end);
+nx = up1_1FULL.deg_30.nx;
+ny = up1_1FULL.deg_30.ny;
 mask_ind = up1_1FULL.deg_30.mask_inds;
 
 %%% Stack the data, remove mask, and replace outliers
-[X] = stackPCA(u,[],mask_ind);
+[X] = stackPCA(u,v,nx, ny, mask_ind);
 
 %%% Put back mask NaNs, unstack the data
-[u_new] = unstackPCA(X,u,[],mask_ind);
+%[u_new] = unstackPCA(X,u,[],mask_ind);
 
 
